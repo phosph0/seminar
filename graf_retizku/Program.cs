1d6d1bb89b0982ce7b855aac3365570bd334e653
phosph0 <belrusbird1@gmail.com>
2025-11-18 00:59:42 +0100

    první část řetízku přátelství
    zpracování vstupu a jeho převedení na graf sousednosti

---
+++ graf_retizku/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace graf_retizku
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet_lidi = Convert.ToInt32(Console.ReadLine());
+            int[,] matice = new int[pocet_lidi + 1, pocet_lidi + 1];
+            string vstupni_data = Console.ReadLine();
+            string[] dvojice = vstupni_data.Split();
+
+            for (int i = 0; i < dvojice.Length; i++)
+            {
+                string[] seznam_dvojice = dvojice[i].Split('-');
+
+                int a = Convert.ToInt32(seznam_dvojice[0]);
+                int b = Convert.ToInt32(seznam_dvojice[1]);
+
+                matice[a, b] = 1;
+                matice[b, a] = 1;
+            }
+            for (int i = 0; i < pocet_lidi + 1; i++)
+            {
+                for (int j = 0; j < pocet_lidi + 1; j++)
+                {
+                    Console.Write($"{matice[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
