4025597c006f75d36709f471a93fc6c024fd94a9
phosph0 <belrusbird1@gmail.com>
2025-12-02 00:42:33 +0100

    druhá část úlohy řetízku přátelství

--- graf_retizku/Program.cs
+++ graf_retizku/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace graf_retizku
 {
 
@@ -6,8 +8,8 @@ namespace graf_retizku
     {
         static void Main(string[] args)
         {
-            int pocet_lidi = Convert.ToInt32(Console.ReadLine());
-            int[,] matice = new int[pocet_lidi + 1, pocet_lidi + 1];
+            int pocetLidi = Convert.ToInt32(Console.ReadLine());
+            int[,] matice = new int[pocetLidi, pocetLidi];
             string vstupni_data = Console.ReadLine();
             string[] dvojice = vstupni_data.Split();
 
@@ -15,21 +17,78 @@ namespace graf_retizku
             {
                 string[] seznam_dvojice = dvojice[i].Split('-');
 
-                int a = Convert.ToInt32(seznam_dvojice[0]);
-                int b = Convert.ToInt32(seznam_dvojice[1]);
+                int a = Convert.ToInt32(seznam_dvojice[0]) - 1;
+                int b = Convert.ToInt32(seznam_dvojice[1]) - 1;
 
                 matice[a, b] = 1;
                 matice[b, a] = 1;
             }
-            for (int i = 0; i < pocet_lidi + 1; i++)
+            //for (int i = 0; i < pocetLidi + 1; i++)
+            //{
+                //for (int j = 0; j < pocetLidi + 1; j++)
+                //{
+                    //Console.Write($"{matice[i, j]} ");
+                //}
+                //Console.WriteLine();
+            //}
+            string [] startCil = Console.ReadLine().Split();
+            int start = Convert.ToInt32(startCil[0]) - 1;
+            int cil = Convert.ToInt32(startCil[1]) - 1;
+
+            bool[] nalezeno = new bool[pocetLidi];
+            int[] predchudce = new int[pocetLidi];
+            nalezeno[start] = true;
+            predchudce[start] = -1;
+            bool existuje = false;
+
+            // seznam pro vzdálenosti
+            // seznam pro předchůdce
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
             {
-                for (int j = 0; j < pocet_lidi + 1; j++)
+                // odebereme vrchol z fronty
+                int vrchol = queue.Dequeue();
+                if (vrchol == cil)
                 {
-                    Console.Write($"{matice[i, j]} ");
+                    existuje = true;
+                    break;
                 }
-                Console.WriteLine();
+
+                // projdeme následníky
+                for (int i = 0; i < pocetLidi; i++)
+                {
+                    if (matice[vrchol, i] == 1 && nalezeno[i]==false)
+                    {
+                        nalezeno[i] = true; // už je nalezený
+                        predchudce[i] = vrchol;
+                        queue.Enqueue(i);
+                    }
+                }
             }
-            int x = 10;
+            if (!existuje)
+            {
+                Console.WriteLine("neexistuje");
+                return;
+            }
+
+            // rekonstrukce cesty
+            List<int> cesta = new List<int>();
+            int aktualni = cil;
+
+            while (aktualni != -1)
+            {
+                cesta.Add(aktualni);
+                aktualni = predchudce[aktualni];
+            }
+
+            for (int i = 0; i < cesta.Count; i++)
+            {
+                cesta[i] = cesta[i] + 1;
+            }
+
+            cesta.Reverse();
+                Console.WriteLine(string.Join(" ", cesta));
+            }
         }
     }
-}
